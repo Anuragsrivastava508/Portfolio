b09036097e9990bdc84ca8f64283f0f7fd2808f7
Anurag srivastava <anuragsrivastava8874@gmail.com>
2026-01-11 01:38:28 +0530

    today work is done

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,21 @@
+
+// function App() {
+//   return (
+//     <div className="min-h-screen flex items-center justify-center">
+//       <h1 className="text-red-600">
+//         Tailwind v4 Working
+//       </h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-red-500">
+        Tailwind FINALLY WORKING 🔥
+      </h1>
+    </div>
+  );
+}
